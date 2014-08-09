a2bb25319347b6ef189e1de3e3e7e5680feb5164
mzaiss <altustro@gmail.com>
2014-08-09 16:00:09 +0200

    tutorial version

--- BATCH_multB1_Z_cw.m
+++ BATCH_multB1_Z_cw.m
@@ -1,5 +1,5 @@
 %% analytic Z-spectra
-%   Date: 2014/08/01 
+%   Date: 2014/08/01
 %   Version for CEST-sources.de
 %   Author: Moritz Zaiss  - m.zaiss@dkfz.de
 %   CEST sources  Copyright (C) 2014  Moritz Zaiss
@@ -18,76 +18,88 @@
 clearvars P  Pstart
 clc
 % setup pool system parameters
-    %water pool 'a'
-    P.R1A=1/3;          % longitudinal relaxation rate 1/T1 of pool a  [s^-1] 
-    P.R2A=2;            % transversal relaxation rate 1/T2 of pool a  [s^-1] 
-    P.dwA=0;            % chemical shift of the water pool in [ppm] 
+%water pool 'a'
+P.R1A=1/2.85;          % longitudinal relaxation rate 1/T1 of pool a  [s^-1]
+P.R2A=2;            % transversal relaxation rate 1/T2 of pool a  [s^-1]
+P.dwA=0;            % chemical shift of the water pool in [ppm]
 
-    % CEST pool 'b'
-    P.fB=0.0018018;     % proton fraction fB=M0B/M0A, e.g. 50mM creatine in water:  4*50/(2*55.5*1000)=0.0018018;
-    P.kBA=50;            % exchange rate [s^-1]     % corresponds to creatine at ~ 22°C, pH=6.4 in PBS (Goerke et al.)
-    P.dwB=1.9;          % chemical shift of the CEST pool in [ppm] 
-    P.R2B=30;           % transversal relaxation rate 1/T2 of pool b  [s^-1] 
-    P.R1B=1;            % longitudinal relaxation rate 1/T1 of pool b  [s^-1] 
+% CEST pool 'b'
+P.fB=0.0018018;     % proton fraction fB=M0B/M0A, e.g. 50mM creatine in water:  4*50/(2*55.5*1000)=0.0018018;
+P.kBA=50;            % exchange rate [s^-1]     % corresponds to creatine at ~ 22°C, pH=6.4 in PBS (Goerke et al.)
+P.dwB=1.9;          % chemical shift of the CEST pool in [ppm]
+P.R2B=30;           % transversal relaxation rate 1/T2 of pool b  [s^-1]
+P.R1B=1;            % longitudinal relaxation rate 1/T1 of pool b  [s^-1]
 
 % setup sequence parameters
-    P.Zi=1;                 % Z initial, in units of thermal M0, Hyperpol.: 10^4                  
-    P.FREQ=300;             % static B0 field [MHz] ~7T ; ppm and µT are used for offsets and B1, therefore gamma=267.5153 is given in Hz.
-    P.B1=1;                 % irradiation amplitude [µT]
-    P.tp=5;                % pulse duration = saturation time [s]
-    P.xZspec= [-4:0.1:4];   % chemical shift of the CEST pool in [ppm] 
+P.Zi=1;                 % Z initial, in units of thermal M0, Hyperpol.: 10^4
+P.FREQ=400;             % static B0 field [MHz] ~7T ; ppm and µT are used for offsets and B1, therefore gamma=267.5153 is given in Hz.
+P.B1=1;                 % irradiation amplitude [µT]
+P.tp=10;                % pulse duration = saturation time [s]
+P.xZspec= [-4:0.1:4];   % chemical shift of the CEST pool in [ppm]
 
-    Pstart=P;
+Pstart=P;
 
 %the parameters  P.dwA,P.R2A,P.dwB,P.kBA,P.fB,P.R2B will be optimized
 % all other paramters will be set to the above defined value in P
 % especially P.R1A is provided to the fit, it needs thus to be measured or assumed
 
-%% OPTION 1: read data from open figure
+
+
+
+%% OPTION 1: get test data from simulation, add some Rician noise
+xdw = [];   yB1 = [];
+P.B1=[0.25 0.5 1 2 4];
+for ii = 1:numel(P.B1);   %% create grid (dw,B1)
+    xdw       = [xdw ; P.xZspec(:)];
+    yB1     = [yB1 ; P.xZspec(:)*0+P.B1(ii)];
+end
+
+[Zstart]=Z_cw_grid(xdw,yB1, P.dwA,P.R2A,P.dwB,P.kBA,P.fB,P.R2B,P);
+Z=ricernd(Zstart,0.01); % add some Rician noise
+
+% Plot fit of start values and data
+figure(1), plot3(xdw,yB1,Zstart,'g-') ; view(180, 0); hold on;
+figure(1), plot3(xdw,yB1,Z,'.') ;  hold on; legend ({'start-model','data'});
+set(gca,'XDir','reverse'); xlabel('\Delta\omega [ppm]'); zlabel('Z(\Delta\omega)'); set(gca,'zLim',[0 1]);
+%% simultaneous multi-B1-Z-spectra fit (requires curve fitting toolbox)
+[fitres, gof] = Multi_B1_Fit(xdw, yB1,Z,P);
+
+
+
+
+%% OPTION 2: read data from open figure
 %  !!! to make sure the script uses the correct figure, close all other !!!
 %  !!!!!!! last plot will be read first, adapt your B1 vector !!!!!!!!
-% reset data 
+% reset data
 
-Z       = [];
-P.xZspec  = [];
-clear xxx zzz
-obj = get(gca,'Children');         
+Z = [];  xdw = [];   yB1 = []; clear xxx zzz
+obj = get(gca,'Children');
+
+P.B1=[ 3 2 1.5 1 0.75 0.5 0.25];  % these are the B1 values of the individual loaded Z-spectra in reverse order.
 
 for ii = 1:numel(obj);
-    xxx=get(obj(ii),'XDATA');               
-    zzz=get(obj(ii),'YDATA');
-    if isrow(xxx)  xxx = xxx';  end
-    if isrow(zzz)  zzz = zzz';  end    
-    Z       = [Z ; zzz];
-    P.xZspec  = [P.xZspec ; xxx];
+    xfig=get(obj(ii),'XDATA');  % get x data from figure
+    zfig=get(obj(ii),'YDATA');  % get Z data from figure
+    if isrow(xfig)  xfig = xfig';  end
+    if isrow(zfig)  zfig = zfig';  end
+    Z           = [Z ; zfig];
+    xdw         = [xdw ; xfig];
+    yB1         = [yB1 ;  xfig*0+P.B1(ii)];
 end
-P.B1=[ 6 5 4 3 2];  % these are the B1 values of the individual loaded Z-spectra in reverse order.
-[x B1]=grid_prep(xxx,P.B1);
-[xData, yData, zData] = prepareSurfaceData( x, B1, B1 );
-[Zstart]=Z_cw_grid(xData,yData, P.dwA,P.R2A,P.dwB,P.kBA,P.fB,P.R2B,P);
-clear obj i zzz 
 
-%% OPTION 2: get test data from simulation, add some Rician noise
-P.B1=[1 2 4];
-[x, B1]=grid_prep(P.xZspec ,P.B1);
-[xData, yData, zData] = prepareSurfaceData( x, B1, B1 );
-[Zstart]=Z_cw_grid(xData,yData, P.dwA,P.R2A,P.dwB,P.kBA,P.fB,P.R2B,P);
+[Zstart]=Z_cw_grid(xdw,yB1, P.dwA,P.R2A,P.dwB,P.kBA,P.fB,P.R2B,P);
+clear obj i zzz
+% Plot fit of start values and data
+figure(1), plot3(xdw,yB1,Zstart,'r-') ; view(180, 0); hold on;
+figure(1), plot3(xdw,yB1,Z,'.') ;  hold on;
 
-Z=ricernd(Zstart,0.01); % add some Rician noise
-%% Plot fit of start values and data
+% simultaneous multi-B1-Z-spectra fit (requires curve fitting toolbox)
+[fitres, gof] = Multi_B1_Fit(xdw, yB1,Z,P);
 
-figure(1), plot3(xData,yData,Zstart,'r-') ; view(180, 0); hold on;
-figure(1), plot3(xData,yData,Z,'.') ;  hold on;
-%%
-[fitres, gof] = Multi_B1_Fit(xData, yData,Z,P)
 
-coeff_names=coeffnames(fitres);
-coeffs=coeffvalues(fitres);
-coeffconf=confint(fitres,0.95);
 
-out{1} = '\Delta\omega [ppm]';
-for ii=1:numel(coeffs) 
-    out{ii+1} = (sprintf('%s=%.5f+-%.5f  (real: %.5f)',coeff_names{ii},coeffs(ii),coeffconf(2,ii)-coeffs(ii),P.(coeff_names{ii})));
-end; 
-xlabel(out); grid off;
-set(gca,'XDir','reverse'); zlabel('Z(\Delta\omega)'); set(gca,'zLim',[0 1]);
+
+
+
+
+
--- Multi_B1_Fit.m
+++ Multi_B1_Fit.m
@@ -11,13 +11,12 @@
 %    You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %   **********************************
 
-function [fitresult, gof] = Multi_B1_Fit(xZspec,B1,z,P)
+function [fitresult, gof] = Multi_B1_Fit(xdw,yB1,Z,P)
 
-%% Fit: 'untitled fit 1'.
-[xData, yData, zData] = prepareSurfaceData( xZspec, B1, z );
-
+% create fit function of depending variables
 piecewiseZ_cw = @(dwA,R2A,dwB,kBA,fB,R2B,x,y) Z_cw_grid(x,y,dwA,R2A,dwB,kBA,fB,R2B,P);
 
+if license('test','curve_fitting_toolbox')
 % Set up fittype and options.
 ft = fittype(piecewiseZ_cw, 'independent', {'x', 'y'}, 'dependent', 'z' );
 opts = fitoptions( ft );
@@ -27,20 +26,58 @@ opts.StartPoint = [0 1 2 2 0.1 1];
 opts.Upper = [Inf Inf Inf Inf Inf Inf];
 
 % Fit model to data.
-[fitresult, gof] = fit( [xData, yData], zData, ft, opts );
+[fitresult, gof] = fit( [xdw, yB1], Z, ft, opts );
 % 
+elseif license('test','optimization_toolbox')
+    error('Code for optimization toolbox not yet implemented');
+%     xyData = {xData,yData};
+%     
+%     [x,resnorm,~,exitflag,output] = lsqcurvefit(piecewiseZ_cw,[0 1 2 2 0.1 1],xyData,zData)
+%     
+%     fitresult.dwA=x(1);
+%     fitresult.R2A=x(2);
+%     fitresult.dwB=x(3);
+%     fitresult.kBA=x(4);
+%     fitresult.fB =x(5);
+%     fitresult.R2B=x(6);
+else
+    error('You dont have the fitting or optimization toolbox');
+end;
+
 % Plot fit with data.
 
-figure(32), plot3(xData,yData,zData,'.b') ;   hold on;
+figure(32),  plot3(xdw,yB1,Z,'.b') ;   hold on; view(180, 0);
 
-[Zfit]=Z_cw_grid(xData,yData,fitresult.dwA,fitresult.R2A,fitresult.dwB,fitresult.kBA,fitresult.fB,fitresult.R2B,P);
-figure(32), plot3(xData,yData,Zfit,'-r') ;   hold on;
-
+[Zfit]=Z_cw_grid(xdw,yB1,fitresult.dwA,fitresult.R2A,fitresult.dwB,fitresult.kBA,fitresult.fB,fitresult.R2B,P);
+figure(32), plot3(xdw,yB1,Zfit,'-r') ;   hold on; 
 legend('data','fit', 'Location', 'NorthEast' );
-% Label axes
-xlabel( 'x' );
-ylabel( 'B1' );
-zlabel( 'Z' );
-grid on
+
+
+%label axes
+if license('test','curve_fitting_toolbox')
+    
+    
+    
+    coeff_names=coeffnames(fitresult);
+    coeffs=coeffvalues(fitresult);
+    coeffconf=confint(fitresult,0.95);
+    out{1} = '\Delta\omega [ppm]';
+    for ii=1:numel(coeffs)
+        out{ii+1} = (sprintf('%s=%.5f+-%.5f  (real: %.5f)',coeff_names{ii},coeffs(ii),coeffconf(2,ii)-coeffs(ii),P.(coeff_names{ii})));
+    end;
+    xlabel(out); grid off;
+    set(gca,'XDir','reverse'); zlabel('Z(\Delta\omega)'); set(gca,'zLim',[0 1]); view(180, 0);
+    
+    
+    
+elseif license('test','optimization_toolbox')
+    
+    error('Code for optimization toolbox not yet implemented');
+    
+else
+    
+    error('You dont have the fitting or optimization toolbox');
+    
+end;
 
 
